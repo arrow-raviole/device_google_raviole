5eecec58e4a8193b47e86192c20a8eda5c32c658
Alessandro Astone <ales.astone@gmail.com>
2021-03-30 11:34:38 +0200

    Build PowerShare HAL
    Change-Id: I3c0321b844acb5f97ac398acc842501410a44b6c

--- device-custom.mk
+++ device-custom.mk
@@ -12,6 +12,9 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-custom
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
 
+# PowerShare
+include hardware/google/pixel/powershare/device.mk
+
 # Build necessary packages for vendor
 
 # Audio
