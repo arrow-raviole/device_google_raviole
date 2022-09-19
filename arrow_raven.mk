1e67364fd964aeefeaa1f75dd10d4ae41054ab14
Alvin Francis <nivlafx@gmail.com>
2022-09-19 19:34:04 -0400

    Changes for Arrow

--- arrow_raven.mk
+++ arrow_raven.mk
@@ -1,11 +1,15 @@
 #
+# Copyright (C) 2022 The ArrowOS Project
 # Copyright (C) 2021 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Arrow stuff.
+$(call inherit-product, vendor/arrow/config/common.mk)
+
+# Inherit Stock Google Camera & Photos.
+$(call inherit-product, vendor/google/camera/googlecamera.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/raviole/aosp_raven.mk)
@@ -15,16 +19,20 @@ $(call inherit-product, device/google/raviole/device-lineage.mk)
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 6 Pro
-PRODUCT_NAME := lineage_raven
+PRODUCT_NAME := arrow_raven
 
 # Boot animation
-TARGET_SCREEN_HEIGHT := 3120
-TARGET_SCREEN_WIDTH := 1440
+TARGET_BOOT_ANIMATION_RES := 1080
+
+#Build with Gapps
+ARROW_GAPPS := true
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=raven \
     PRIVATE_BUILD_DESC="raven-user 13 TP1A.220905.004 8927612 release-keys"
 
 BUILD_FINGERPRINT := google/raven/raven:13/TP1A.220905.004/8927612:user/release-keys
+
+PRODUCT_OVERRIDE_GMS_FINGERPRINT := google/raven/raven:13/TP1A.220905.004/8927612:user/release-keys
 
 $(call inherit-product, vendor/google/raven/raven-vendor.mk)
