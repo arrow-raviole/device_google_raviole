bfe8c6db575e59f2a2304998bc476c3792b4c71d
Alvin Francis <nivlafx@gmail.com>
2022-09-20 21:22:05 -0400

    AICP changes

--- aicp_raven.mk
+++ aicp_raven.mk
@@ -1,11 +1,15 @@
 #
+# Copyright (C) 2022 The AICP Project
 # Copyright (C) 2021 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+ifeq (aicp_raven,$(TARGET_PRODUCT))
+
+# Inherit some common AICP stuff.
+$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/raviole/aosp_raven.mk)
@@ -15,7 +19,7 @@ $(call inherit-product, device/google/raviole/device-lineage.mk)
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 6 Pro
-PRODUCT_NAME := lineage_raven
+PRODUCT_NAME := aicp_raven
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 3120
@@ -25,6 +29,11 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=raven \
     PRIVATE_BUILD_DESC="raven-user 13 TP1A.220905.004 8927612 release-keys"
 
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    DEVICE_MAINTAINERS="Alvin F. (nivlafx & mikeNG)"
+
 BUILD_FINGERPRINT := google/raven/raven:13/TP1A.220905.004/8927612:user/release-keys
 
 $(call inherit-product, vendor/google/raven/raven-vendor.mk)
+
+endif
